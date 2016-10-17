dbb5bf3463be4fde83f25607e49db38dcd992e0b
Andres Norberto Schuster <andi.schus@live.com.ar>
2016-10-17 11:07:51 -0300

    Legales en "Acerca de..."
    Se agrego un link con terminos y condiciones, el cual activa un modal
    donde se muestran todos los terminos y condiciones almacenados en la
    base de datos.

---
+++ acercade.php
@@ -0,0 +1,32 @@
+<?php
+
+include_once 'configure.php';
+
+function cargarModalLegales() {
+
+    $con = new Conexion();
+    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    try {
+        $query = $con->prepare("SELECT * FROM " . tabla_legales);
+        $query->execute();
+        //var_dump($query);
+        $result = $query->fetchAll(PDO::FETCH_ASSOC);
+        //var_dump($result);
+        $txt = "";
+        foreach ($result as $row) {
+            $titulo = utf8_encode($row['titulo']);
+            $desc = utf8_encode($row['descripcion']);
+            $legales[] = array('titulo' => $titulo, 'descripcion' => $desc);
+        }
+        //var_dump($legales);
+        $jsonlegales = json_encode(array_values($legales));
+        //var_dump($jsonlegales);
+        echo $jsonlegales;
+        $con = null;
+    } catch (Exception $e) {
+        echo 'Error: ' . $e->getMessage();
+    }
+}
+
+cargarModalLegales();
+?>
--- js/acercade.js
+++ js/acercade.js
@@ -1,11 +1,11 @@
 function textoAcercaDe(){
-    $("#txtTitulo").html("¿Que es CeliAPP?");
+    $("#txtTitulo").html("ï¿½Que es CeliAPP?");
     $("#txtAcercaDe").html("Acerca de CeliAPP");
-    $("#txtDesc").html("<p>CeliAPP nació en Argentina como una herramienta útil para la comunidad,  luego de conocer el día a día y la problemática que atraviesan nuestros allegados con celiaquía.</p><p>La dificultad de encontrar los productos de manera sencilla o la necesidad de  utilizar múltiples servicios para obtener los resultados esperados, hace que sea muy frustrante y tedioso para las personas con esta condición.</p><p>Es por eso que desde CeliAPP queremos ayudar a hacer esta tarea mucho más amena, aportando agilidad en la búsqueda, ofreciendo  orientación sobre los posibles lugares de compra y precios de referencia que los mismos usuarios pueden agregar asegurando un compromiso  para el resto de la comunidad.</p>");
+    $("#txtDesc").html("<p>CeliAPP naciÃ³ en Argentina como una herramienta Ãºtil para la comunidad,  luego de conocer el dÃ­a a dÃ­a y la problemÃ¡tica que atraviesan nuestros allegados con celiaquÃ­a.</p><p>La dificultad de encontrar los productos de manera sencilla o la necesidad de  utilizar mÃºltiples servicios para obtener los resultados esperados, hace que sea muy frustrante y tedioso para las personas con esta condiciÃ³n.</p><p>Es por eso que desde CeliAPP queremos ayudar a hacer esta tarea mucho mÃ¡s amena, aportando agilidad en la bÃºsqueda, ofreciendo  orientaciÃ³n sobre los posibles lugares de compra y precios de referencia que los mismos usuarios pueden agregar asegurando un compromiso  para el resto de la comunidad.</p>");
 };
 
 function textoDesarrolladores(){
-    $("#txtTitulo").html("¿Quienes desarrollamos CeliApp?");
+    $("#txtTitulo").html("ï¿½Quienes desarrollamos CeliApp?");
     $("#txtAcercaDe").html("Acerca de Desarrolladores de CeliAPP");
     $("#txtDesc").html("<p>Schuster Andres Norberto</p><p>Medina Matias</p><p>Soria Maximiliano</p><p>Escudero Cristian</p><p>Venica Roman</p><p>Piriz Fernando</p>");
 };
@@ -13,5 +13,37 @@ function textoDesarrolladores(){
 function textoSoftware(){
     $("#txtTitulo").html("Catedra de Desarrollo de Software");
     $("#txtAcercaDe").html("Acerca de Desarrollo de Software");
-    $("#txtDesc").html("Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas Letraset, cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas Letraset, las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas Letraset, las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ");
+    $("#txtDesc").html("Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estÃ¡ndar de las industrias desde el aÃ±o 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usÃ³ una galerÃ­a de textos y los mezclÃ³ de tal manera que logrÃ³ hacer un libro de textos especimen. No sÃ³lo sobreviviÃ³ 500 aÃ±os, sino que tambien ingresÃ³ como texto de relleno en documentos electrÃ³nicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creaciÃ³n de las hojas Letraset, cuales contenian pasajes de Lorem Ipsum, y mÃ¡s recientemente con software de autoediciÃ³n, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum que logrÃ³ hacer un libro de textos especimen. No sÃ³lo sobreviviÃ³ 500 aÃ±os, sino que tambien ingresÃ³ como texto de relleno en documentos electrÃ³nicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creaciÃ³n de las hojas Letraset, las cuales contenian pasajes de Lorem Ipsum, y mÃ¡s recientemente con software de autoediciÃ³n, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum que logrÃ³ hacer un libro de textos especimen. No sÃ³lo sobreviviÃ³ 500 aÃ±os, sino que tambien ingresÃ³ como texto de relleno en documentos electrÃ³nicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creaciÃ³n de las hojas Letraset, las cuales contenian pasajes de Lorem Ipsum, y mÃ¡s recientemente con software de autoediciÃ³n, como por ");
 };
+
+$(document).ready(function(){
+    $("#aLegales").on('click', function(){
+        $.ajax({
+            url: 'acercade.php',
+            method: 'POST',
+            success: function(response){
+                //console.log(response);
+                var texto = JSON.parse(response);
+                //console.log(texto);
+                var txt = "";
+                var i = 0;
+                $("#modalTitle").html(texto[i].titulo);
+                for (i=0; i<texto.length-1; i++){
+                    console.log(texto[i].titulo);
+                    console.log(texto[i].descripcion);
+                    txt += "<p>";
+                    txt += texto[i].descripcion;
+                    txt += "</p>";
+                    i++;
+                    txt += "<h4>";
+                    txt += texto[i].titulo;
+                    txt += "</h4>";
+                    i--;
+                }
+                txt += "<p>" + texto[texto.length-1].descripcion + "</p>";
+                $("#modalDesc").html(txt);
+                $("#divModal").modal('show');
+            }
+        });
+    });
+});
