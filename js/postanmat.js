1ed03e2fdbf42dc849fc8240e6a5d374ded7f5cf
Andres Norberto Schuster <andi.schus@live.com.ar>
2016-11-03 16:31:25 -0300

    Actualizar BD (funciona con un bug)
    La base de datos se actualiza, pero cuando el flujo vuelve del php, no
    ingresa al success del ajax, ni a ningun lado.

--- js/postanmat.js
+++ js/postanmat.js
@@ -5,19 +5,7 @@ $(document).ready(function () {
 
 function cargarDatos() {
     var tabla = $("#GridView1 > tbody > tr:gt(0)");
-    //data = '{"data": "';
-    data = '';
-    /*jsontabla = '{';
-     tabla.each(function () {
-     jsontabla += '{categoria: ' + this.cells[0].innerHTML.replace(/,/g, " ") + ', ';
-     jsontabla += 'marca: ' + this.cells[1].innerHTML.replace(/,/g, " ") + ', ';
-     jsontabla += 'nombrefantasia: ' + this.cells[2].innerHTML.replace(/,/g, " ") + ', ';
-     jsontabla += 'descripcion: ' + this.cells[3].innerHTML.replace(/,/g, " ") + ', ';
-     jsontabla += 'rnpa: ' + this.cells[4].innerHTML.replace(/,/g, " ") + '}, ';
-     });
-     jsontabla = jsontabla.slice(0,-2);
-     jsontabla += '}';
-     */
+    data = "";
     var rnpa = ""
     tabla.each(function () {
         if (rnpa != this.cells[4].innerHTML.replace(/,/g, " ").replace(/"/g, " ")) {
@@ -31,15 +19,14 @@ function cargarDatos() {
     });
 
     data = data.slice(0, -2);
-//     data += '"';
 
-    console.log(data);
+//    console.log(data);
 
     $.ajax({
         data: {data: data},
         dataType: "json",
-        url: 'actualizarListado.php',
-        type: 'POST',
+        url: "actualizarListado.php",
+        type: "POST",
         success: function (response) {
             console.log("La base de datos se ha actualizado con éxito");
             console.log(response);
