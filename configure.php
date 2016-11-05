a3c13f2b659ea015b38c8b6d077efa6699a4185a
MedinaMatiasDaniel <matiasdanielmedina86@gmail.com>
2016-11-04 22:50:03 -0300

    Arreglo Configure
    Se arreglo la estructura del configure y se agregaron datos de los dos
    host en uso

--- configure.php
+++ configure.php
@@ -1,16 +1,26 @@
 <?php
 
 require_once('conexion.php');
-/*
-  define("database", "u602243947_celia");
-  define("host", "mysql.hostinger.com.ar");
-  define("user", "u602243947_root");
-  define("password", "celiappg12016");
- */
+
+//------------Datos Host-----------------
+  //define("password", "celiappg12016");
+  //define("host", "mysql.hostinger.com.ar");
+
+//------------Host Produccion------------
+  //define("database", "u390477896_celia");
+  //define("user", "u390477896_root");
+
+//------------Host QA--------------------
+//  define("database", "u602243947_celia");
+//  define("user", "u602243947_root");
+
+//------------Datos LocalHost------------
 define("database", "celiapp");
 define("host", "localhost");
 define("user", "root");
 define("password", "");
+
+//------------Datos de la base de datos---
 define("lista_ubicacion", "producto_precio");
 define("tabla_productos", "producto");
 define("tabla_usuarios", "usuario");
