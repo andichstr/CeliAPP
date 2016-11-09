afdbd06348b53431a813d761120ad68ef4c9f3e1
MedinaMatiasDaniel <matiasdanielmedina86@gmail.com>
2016-11-09 18:28:08 -0300

    Agregado de icono
    Agregado de icono, intento de muestra de productos en el mapa

--- js/generarMapa.js
+++ js/generarMapa.js
@@ -32,6 +32,16 @@ function initMap(coordenadas) {
         title: "arrastrame"
     });
 
+    var locations = [
+        ['loan 1', -34.789, -58.523, 'address 1'],
+        ['loan 2', -34.789, -58.524, 'address 2'],
+        ['loan 3', -34.836, -58.494, 'address 3'],
+        ['loan 4', -34.850, -58.514, 'address 4'],
+        ['loan 5', -34.847, -58.506, 'address 5']
+    ];
+    
+    
+
     autocomplete.addListener('place_changed', function () {
         infowindow.close();
         marker.setVisible(false);
@@ -46,14 +56,14 @@ function initMap(coordenadas) {
             map.fitBounds(place.geometry.viewport);
         } else {
             map.setCenter(place.geometry.location);
-            map.setZoom(17);  // Why 17? Because it looks good.
+            map.setZoom(15);  // Why 17? Because it looks good.
         }
-        marker.setIcon(/** @type {google.maps.Icon} */({
-            url: place.icon,
+        marker.setIcon(({
+            url: "img/logo25x25.png",
             size: new google.maps.Size(71, 71),
             origin: new google.maps.Point(0, 0),
             anchor: new google.maps.Point(17, 34),
-            scaledSize: new google.maps.Size(35, 35)
+            scaledSize: new google.maps.Size(25, 25)
         }));
         marker.setPosition(place.geometry.location);
         marker.setVisible(true);
@@ -67,12 +77,27 @@ function initMap(coordenadas) {
             ].join(' ');
         }
         google.maps.event.addListener(marker, 'dragend', function (evt) {
-            console.log(evt.latLng.lat().toFixed(3) + ',' + evt.latLng.lng().toFixed(3));
-            infowindow.setContent('<div class="marcador"><strong>' + place.name + '</strong><br>' + address);
+            var latlng1 = evt.latLng.lat().toFixed(3) + ',' + evt.latLng.lng().toFixed(3);
+            console.log (latlng1);
+            infowindow.setContent('<div class="marcador"><strong>' + place.address_components + '</strong><br>' + address);
             infowindow.open(map, marker);
         });
 
     })
+
+function getAddress(myLatitude,myLongitude) {
+			
+			var geocoder	= new google.maps.Geocoder();				// create a geocoder object
+			var location	= new google.maps.LatLng(myLatitude, myLongitude);	// turn coordinates into an object 			
+			geocoder.geocode({'latLng': location}, function (results, status) {
+				if(status == google.maps.GeocoderStatus.OK) {			// if geocode success
+					processAddress(results[0].formatted_address);		// if address found, pass to processing function
+				} else {
+				  alert("Geocode failure: " + status);				// alert any other error(s)
+				  return false;
+				}
+			});
+		}
 
 }
 
@@ -110,16 +135,50 @@ function errorMapa(error) {
 }
 ;
 
+function setMarkers(map, locations) {
+
+    var marker, i
+
+    for (i = 0; i < locations.length; i++)
+    {
+
+        var loan = locations[i][0]
+        var lat = locations[i][1]
+        var long = locations[i][2]
+        var add = locations[i][3]
+
+        latlngset = new google.maps.LatLng(lat, long);
+
+        var marker = new google.maps.Marker({
+            map: map, title: loan, position: latlngset
+        });
+        map.setCenter(marker.getPosition())
+
+
+        var content = "Loan Number: " + loan + '</h3>' + "Address: " + add
+
+        var infowindow = new google.maps.InfoWindow()
+
+        google.maps.event.addListener(marker, 'click', (function (marker, content, infowindow) {
+            return function () {
+                infowindow.setContent(content);
+                infowindow.open(map, marker);
+            };
+        })(marker, content, infowindow));
+
+    }
+}
+
 $(document).ready(function () {
-    $("#pac-input").keyup(function(){
+    $("#pac-input").keyup(function () {
         $("#txtUbicacion").val($("#pac-input").val());
     });
-    $("#txtUbicacion").keyup(function(){
+    $("#txtUbicacion").keyup(function () {
         $("#pac-input").val($("#txtUbicacion").val());
     });
-    
-    
-    $("#formMapa").on('submit', function(event){
+
+
+    $("#formMapa").on('submit', function (event) {
         event.preventDefault();
     });
 });
